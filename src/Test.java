efb61fb47590bc46291988f0b018d0f55ae3dc54
norway <147002984+Vishalpatel91912@users.noreply.github.com>
2024-01-27 15:09:39 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("My Name is Patel");
+        System.out.println("My Name is Patel, How are you ? Khana kha ke jana");
     }
 }
