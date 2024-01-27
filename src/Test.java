c603022f81b506470692134e8c1f7b72074499eb
vishalpatel91912 <vishalpatel91912@gmail.com>
2024-01-27 15:14:12 +0000

    commit2

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
+        System.out.println("My Name is Patel, Hello");
         System.out.println("My Name is Patel, How are you ? Khana kha ke jana");
     }
 }
